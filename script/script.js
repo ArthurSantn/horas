7a13d984c2043f12d650ada9e1b0394603c4843b
Arthur <138633021+ArthurSantn@users.noreply.github.com>
2023-09-22 19:46:46 -0300

    site funcionando

--- script/script.js
+++ script/script.js
@@ -1,18 +1,24 @@
 function carregar() {
 
     var msg = window.document.getElementById('hora')
-    var img = window.document.getElementById('imagem')
+    var img = window.document.getElementById('img')
 
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas `
+    var min = data.getMinutes() 
+
+    msg.innerHTML = `Agora são ${hora} horas e ${min} minutos`
 
     if (hora >= 0 && hora < 12) {
-        img.src= '../imagens/diar.png'
+        img.src= './imagens/diar.png'
     } else if (hora >= 12 && hora < 18) {
-        img.src= '../imagens/tarder.png'
+        img.src= './imagens/tarder.png'
+        document.body.style.backgroundAttachment= "fixed"
+        document.body.style.backgroundImage= "linear-gradient(to bottom, #265D73, #BF9075)"
     } else {
-        img.src= '../imagens/noiter.png'
+        img.src= './imagens/noiter.png'
+        document.body.style.backgroundAttachment= "fixed"
+        document.body.style.backgroundImage= "linear-gradient(to bottom, #456A73, #072126)"
     }
 }
 
